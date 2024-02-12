71707e1368b90ea671b691618d154fdba13c857a
fahryanputra <fahryanputra@gmail.com>
2024-02-12 10:57:57 +0700

    Add scroll feature for task list

--- src/components/ui/UI.js
+++ src/components/ui/UI.js
@@ -1,9 +1,13 @@
 import renderSidebar from "UI/Sidebar";
+import renderMain from "UI/Main";
+import TodoList from "Modules/TodoList";
 import "Templates/style.css";
 
 class UI {
     static renderHomepage() {
+        const todoList = new TodoList();
         renderSidebar();
+        renderMain(todoList.getProject("Inbox"));
     }
 }
 
